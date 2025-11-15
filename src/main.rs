8f8efa2825c881996890379c2ef7a4fdebba851e
Ho1Ai desktop <koekto33309@gmail.com>
2025-11-15 00:06:36 +0000

    small structure update. Parallely writing backend

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod defaultStructs;
+mod projectInfo;
 
 use std::env;
 
 fn main() {
     let args_list: Vec<String> = env::args().collect();
+
 
     //println!("Hello, world!");
 }
---
+++ src/projectInfo/mod.rs
@@ -0,0 +1,4 @@
+pub mod initializeRoot;
+mod checkPotentialRoot;
+mod readInfo;
+mod reqSender;
