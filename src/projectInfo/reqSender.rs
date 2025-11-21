5a9fbac1018a4c83e5c82d9d70e411dba50c7e78
Ho1Ai desktop <koekto33309@gmail.com>
2025-11-21 00:04:35 +0000

    decided to use reqwest

--- src/projectInfo/reqSender.rs
+++ src/projectInfo/reqSender.rs
@@ -1,2 +1,9 @@
 //IDK what to use... Maybe Reqwest
 //Yeah, I'll go for reqwest
+use crate::defaultStructs;
+
+pub async fn getPackageInfo (pkg_name: String) /*-> defaultStructs::PackageEntity*/ -> Result<String, &'static str> {
+    let response = reqwest::get("http://localhost:8000/api/get-pkg-info").await?.text().await?;
+
+    Result::Ok(response)
+}
